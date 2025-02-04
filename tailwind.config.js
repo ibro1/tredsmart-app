df55ff41d98ceaa8617b9b4152a25efc406f6b2f
Abubakar <abubakaribro1@gmail.com>
2025-02-04 14:25:19 +0100

    css6

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,jsx,ts,tsx}",
-    "./**/*.{js,jsx,ts,tsx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
